66726e2e1ffc1a8a0d106bf978b9d88d605637dc
Dante Souza e Souza <lokinmodar@gmail.com>
2020-06-10 22:07:08 -0300

    Initial Commit

---
+++ AProf/src/Models/Aluno.java
@@ -0,0 +1,74 @@
+package Models;
+
+import java.util.Date;
+import java.util.List;
+
+public class Aluno {
+	int ra;
+	String nome;
+	String rg;
+	String cpf;
+	Date dtnasc;
+	List<Telefone> telefones;
+	
+	public Aluno(int ra, String nome, String rg, String cpf, Date dtnasc, List<Telefone> telefones) {
+		super();
+		this.ra = ra;
+		this.nome = nome;
+		this.rg = rg;
+		this.cpf = cpf;
+		this.dtnasc = dtnasc;
+		this.telefones = telefones;
+	}
+
+	public int getRa() {
+		return ra;
+	}
+
+	public void setRa(int ra) {
+		this.ra = ra;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDtnasc() {
+		return dtnasc;
+	}
+
+	public void setDtnasc(Date dtnasc) {
+		this.dtnasc = dtnasc;
+	}
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
+	}
+	
+	
+
+}
---
+++ AProf/src/Models/Telefone.java
@@ -0,0 +1,15 @@
+package Models;
+
+public class Telefone {
+	private String numero;
+	private String tipo;
+	// Enum tipo{ "Comercial", "Residencial", "Celular" };
+	
+	public Telefone(String numero, String tipo) {
+		super();
+		this.numero = numero;
+		this.tipo = tipo;
+	}
+	
+
+}
